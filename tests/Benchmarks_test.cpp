aa0444bdd8a34b0ca09ff782029f2d3b277683f1
David Wicks <wicks.db@gmail.com>
2014-11-23 10:38:56 -0500

    Benchmarks for Sequence creation/slicing.

--- tests/Benchmarks_test.cpp
+++ tests/Benchmarks_test.cpp
@@ -10,7 +10,8 @@
 #include <chrono>
 
 using namespace std;
-using namespace cinder;
+using cinder::Timeline;
+using cinder::vec2;
 using namespace choreograph;
 
 using timer = chrono::high_resolution_clock;
@@ -43,7 +44,69 @@ void printHeading( const std::string &text )
   cout << string( text.size(), '=' ) << endl;
 }
 
-TEST_CASE( "Choreograph Timeline Basic Performance" ) {
+TEST_CASE( "Sequence Manipulation Timing" )
+{
+  printHeading( "Sequence Creation and Slicing" );
+
+  auto create_medium_begin = timer::now();
+  Sequence<float> medium_sequence( 0.0f );
+  medium_sequence.then<RampTo>( 10.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 1.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 20.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 2.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 40.0f, 1.0f, EaseInOutQuad() )
+    .then<Hold>( 1000.0f, 0.5f )
+    .then<RampTo>( 4.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 50.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 5.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 60.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 6.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 70.0f, 1.0f, EaseInOutQuad() );
+
+  auto create_medium_end = timer::now();
+  printTiming( "Creating Medium Sequence", create_medium_end - create_medium_begin );
+
+  auto create_auto_medium_begin = timer::now();
+  auto auto_medium_sequence = Sequence<float>( 0.0f )
+    .then<RampTo>( 1.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 20.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 2.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 40.0f, 1.0f, EaseInOutQuad() )
+    .then<Hold>( 1000.0f, 0.5f )
+    .then<RampTo>( 4.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 50.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 5.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 60.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 6.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 70.0f, 1.0f, EaseInOutQuad() )
+    .then<RampTo>( 10.0f, 1.0f, EaseInOutQuad() );
+
+  auto create_auto_medium_end = timer::now();
+  printTiming( "Creating Medium Sequence rhs Assignment", create_auto_medium_end - create_auto_medium_begin );
+
+  auto create_huge_begin = timer::now();
+  Sequence<float> huge_sequence( 0.0f );
+  for( int i = 0; i < 1.0e6; i += 1 ) {
+    huge_sequence.then<RampTo>( cos( i * 0.1 ) * (i - 1.0e3), 1.0f, EaseInOutCubic() );
+  }
+  auto create_huge_end = timer::now();
+  printTiming( "Creating Huge Sequence", create_huge_end - create_huge_begin );
+
+  auto slice_medium_begin = timer::now();
+  auto sub_medium = medium_sequence.slice( 1.3f, 7.2f );
+  auto slice_medium_end = timer::now();
+  printTiming( "Slicing Medium Sequence", slice_medium_end - slice_medium_begin );
+
+  auto slice_huge_begin = timer::now();
+  // Performance is linear on the number of phrases before end time.
+  auto sub_huge = huge_sequence.slice( 5.55f, 15000.025f );
+  auto slice_huge_end = timer::now();
+  printTiming( "Slicing Huge Sequence", slice_huge_end - slice_huge_begin );
+}
+
+TEST_CASE( "Choreograph Timeline Basic Performance" )
+{
+
   ch::Timeline choreograph_timeline;
   choreograph_timeline.setDefaultRemoveOnFinish( false );
   float dt = 1.0f / 60.0f;
@@ -51,63 +114,52 @@ TEST_CASE( "Choreograph Timeline Basic Performance" ) {
 
   printHeading( "Basic Timing for " + to_string( count ) + " Motions" );
   vector<Output<vec2>> targets( count, vec2( 0 ) );
-  {
-    auto begin = timer::now();
-
-    for( auto &target : targets ) {
-      choreograph_timeline.apply( &target ).then<RampTo>( vec2( 10.0f ), 1.0f ).startFn( [] (Motion<vec2> &) {} ).updateFn( [] ( const vec2 &value ) { value + vec2(1); } ).finishFn( [] (Motion<vec2> &) {} );
-    }
-    auto end = timer::now();
-    printTiming( "Creating Motions with Callbacks in place", end - begin );
+  auto create_in_place_begin = timer::now();
+  for( auto &target : targets ) {
+    choreograph_timeline.apply( &target ).then<RampTo>( vec2( 10.0f ), 1.0f ).startFn( [] (Motion<vec2> &) {} ).updateFn( [] ( const vec2 &value ) { value + vec2(1); } ).finishFn( [] (Motion<vec2> &) {} );
   }
+  auto create_in_place_end = timer::now();
+  printTiming( "Creating Motions with Callbacks in place", create_in_place_end - create_in_place_begin );
 
-  {
-    auto begin = timer::now();
-    for( int i = 0; i < 60; ++i ) {
-      choreograph_timeline.step( dt );
-    }
-    auto end = timer::now();
-    printTiming( "60 Motion Steps (1sec at 60Hz)", end - begin );
-  }
-
-  {
-    auto begin = timer::now();
-    for( auto &target : targets ) {
-      target.disconnect();
-    }
-
-    auto end = timer::now();
-    printTiming( "Disconnecting Motions", end - begin );
-  }
-
-  {
-    auto begin = timer::now();
+  auto step_created_in_place_begin = timer::now();
+  for( int i = 0; i < 60; ++i ) {
     choreograph_timeline.step( dt );
+  }
+  auto step_created_in_place_end = timer::now();
+  printTiming( "60 Motion Steps (1sec at 60Hz)", step_created_in_place_end - step_created_in_place_begin );
 
-    auto end = timer::now();
-    printTiming( "Step to remove disconnected Motions", end - begin );
+  auto disconnect_begin = timer::now();
+  for( auto &target : targets ) {
+    target.disconnect();
   }
 
-  {
-    auto begin = timer::now();
-    Sequence<vec2> sequence( vec2( 1.0f ) );
-    sequence.then<RampTo>( vec2( 5.0f ), 1.0f ).then<RampTo>( vec2( 10.0f, 6.0f ), 0.5f );
-    for( auto &target : targets ) {
-      choreograph_timeline.apply( &target, sequence ).startFn( [] (Motion<vec2> &) {} ).updateFn( [] ( const vec2 &value ) { value + vec2(1); } ).finishFn( [] (Motion<vec2> &) {} );
-    }
+  auto disconnect_end = timer::now();
+  printTiming( "Disconnecting Motions", disconnect_end - disconnect_begin );
 
-    auto end = timer::now();
-    printTiming( "Creating Motions from existing Sequence", end - begin );
+  auto step_disconnected_begin = timer::now();
+  choreograph_timeline.step( dt );
+
+  auto step_disconnected_end = timer::now();
+  printTiming( "Step to remove disconnected Motions", step_disconnected_end - step_disconnected_begin );
+
+  auto create_copy_begin = timer::now();
+  Sequence<vec2> sequence( vec2( 1.0f ) );
+  sequence.then<RampTo>( vec2( 5.0f ), 1.0f ).then<RampTo>( vec2( 10.0f, 6.0f ), 0.5f );
+  for( auto &target : targets ) {
+    choreograph_timeline.apply( &target, sequence ).startFn( [] (Motion<vec2> &) {} ).updateFn( [] ( const vec2 &value ) { value + vec2(1); } ).finishFn( [] (Motion<vec2> &) {} );
   }
 
-  {
-    auto begin = timer::now();
-    for( int i = 0; i < 60; ++i ) {
-      choreograph_timeline.step( dt );
-    }
-    auto end = timer::now();
-    printTiming( "60 Motion Steps (1sec at 60Hz)", end - begin );
+  auto create_copy_end = timer::now();
+  printTiming( "Creating Motions from existing Sequence", create_copy_end - create_copy_begin );
+
+
+  auto step_copy_created_begin = timer::now();
+  for( int i = 0; i < 60; ++i ) {
+    choreograph_timeline.step( dt );
   }
+  auto step_copy_created_end = timer::now();
+  printTiming( "60 Motion Steps (1sec at 60Hz)", step_copy_created_end - step_copy_created_begin );
+
 }
 
 TEST_CASE( "Comparative Performance with cinder::Timeline" )
@@ -115,15 +167,15 @@ TEST_CASE( "Comparative Performance with cinder::Timeline" )
   ch::Timeline    choreograph_timeline;
   ci::TimelineRef cinder_timeline = ci::Timeline::create();
 
-  const int             tween_count = 5000;
-  vector<Output<vec2>>  targets( tween_count, vec2( 0 ) );
-  vector<Anim<vec2>>    cinder_targets( tween_count, vec2( 0 ) );
+  const int               tween_count = 5000;
+  vector<Output<vec2>>    targets( tween_count, vec2( 0 ) );
+  vector<ci::Anim<vec2>>  cinder_targets( tween_count, vec2( 0 ) );
 
-  double ci_step_avg = 0;
-  double ch_step_avg = 10;
-  double ci_create_avg = 0;
-  double ch_create_avg = 0;
-  double iterations = 4;
+  double ci_step_avg    = 0;
+  double ch_step_avg    = 0;
+  double ci_create_avg  = 0;
+  double ch_create_avg  = 0;
+  double iterations     = 4;
 
   SECTION( "Tween many targets" )
   {
@@ -192,8 +244,8 @@ TEST_CASE( "Comparative Performance with cinder::Timeline" )
 
     for( int j = 0; j < iterations; j += 1 )
     {
-      Output<vec2>  target( vec2( 0 ) );
-      Anim<vec2>    cinder_target( vec2( 0 ) );
+      Output<vec2>    target( vec2( 0 ) );
+      ci::Anim<vec2>  cinder_target( vec2( 0 ) );
 
       // Create Choreograph Motions
       auto ch_create_begin = timer::now();
