c18107aa18135628016322e51c8052b06396f005
David Wicks <wicks.db@gmail.com>
2014-12-12 13:11:16 -0500

    Move TimelineOptions declarations to separate file.
    Makes Timeline file easier to read.

--- src/choreograph/TimelineOptions.hpp
+++ src/choreograph/TimelineOptions.hpp
@@ -27,10 +27,153 @@
 
 #pragma once
 
+#include "Motion.hpp"
+#include "Cue.h"
+
+namespace choreograph
+{
+
+class Timeline;
+
+///
+/// Options for manipulating newly created TimelineItems.
+/// Uses CRTP so we don't lose the actual type when chaining methods.
+/// Do not store the TimelineOptions object, as it contains a non-owning reference.
+///
+template<typename Derived>
 class TimelineOptions
 {
 public:
-  TimelineOptions();
-  ~TimelineOptions();
+  TimelineOptions( TimelineItem &item ):
+  _item( item )
+  {}
+
+  //=================================================
+  // TimelineItem Interface Mirroring.
+  //=================================================
+
+  /// Set whether the item should be removed from the timeline on finish.
+  Derived& removeOnFinish( bool doRemove ) { _item.setRemoveOnFinish( doRemove ); return self(); }
+
+  /// Set the rate at which time advances for Motion.
+  Derived& playbackSpeed( Time speed ) { _item.setPlaybackSpeed( speed ); return self(); }
+
+  /// Set the initial time offset of the TimelineItem.
+  /// For Cues, this sets the time in the future.
+  /// For Motions, this is akin to adding a hold at the beginning of the Sequence.
+  Derived& setStartTime( Time t ) { _item.setStartTime( t ); return self(); }
+
+  /// Returns a shared_ptr to the control object for the Item. Allows you to cancel the Item later.
+  TimelineItemControlRef  getControl() { return _item.getControl(); }
+
+  /// Returns an object that cancels the Cue when it falls out of scope.
+  /// You should store a ScopedCueRef in any class that captures [this] in a cued lambda.
+  ScopedCancelRef         getScopedControl() { return std::make_shared<ScopedCancel>( _item.getControl() ); }
+
 private:
+  TimelineItem &_item;
+  Derived& self() { return static_cast<Derived&>( *this ); }
 };
+
+///
+/// MotionOptions provide a temporary facade for manipulating a timeline Motion and its underlying Sequence.
+/// All methods return a reference back to the MotionOptions object for chaining.
+/// Do not store the MotionOptions object, as it contains non-owning references.
+///
+template<typename T>
+class MotionOptions : public TimelineOptions<MotionOptions<T>>
+{
+public:
+  using SelfT = MotionOptions<T>;
+  using MotionCallback = typename Motion<T>::Callback;
+
+  MotionOptions( Motion<T> &motion, Sequence<T> &sequence, const Timeline &timeline ):
+  TimelineOptions<MotionOptions<T>>( motion ),
+  _motion( motion ),
+  _sequence( sequence ),
+  _timeline( timeline )
+  {}
+
+  //=================================================
+  // Motion Interface Mirroring.
+  //=================================================
+
+  /// Set function to be called when Motion starts. Receives reference to motion.
+  SelfT& startFn( const MotionCallback &fn ) { _motion.setStartFn( fn ); return *this; }
+
+  /// Set function to be called when Motion updates. Receives current target value.
+  SelfT& updateFn( const typename Motion<T>::DataCallback &fn ) { _motion.setUpdateFn( fn ); return *this; }
+
+  /// Set function to be called when Motion finishes. Receives reference to motion.
+  SelfT& finishFn( const MotionCallback &fn ) { _motion.setFinishFn( fn ); return *this; }
+
+  /// Set a function to be called when the current inflection point is crossed.
+  /// An inflection occcurs when the Sequence moves from one Phrase to the next.
+  /// You must add a phrase after this for the inflection to occur.
+  SelfT& onInflection( const MotionCallback &fn ) { return onInflection( _sequence.getPhraseCount(), fn ); }
+  /// Adds an inflection callback when the specified phrase index is crossed.
+  SelfT& onInflection( size_t point, const MotionCallback &fn ) { _motion.addInflectionCallback( point, fn ); return *this; }
+
+  /// Clip the motion in \t time from the current Motion playhead.
+  /// Also discards any phrases we have already played up to this point.
+  SelfT& cutIn( Time t ) { _motion.cutIn( t ); return *this; }
+
+  /// Clip the motion at time \t from the beginning of the Motion's Sequence.
+  /// When used after Timeline::apply, will have the same effect as cutIn().
+  SelfT& cutAt( Time t ) { _motion.sliceSequence( 0, t ); return *this; }
+
+  //=================================================
+  // Sequence Interface Mirroring.
+  //=================================================
+
+  /// Set the current value of the Sequence. Acts as an instantaneous hold.
+  SelfT& set( const T &value ) { _sequence.set( value ); return *this; }
+
+  /// Construct and append a Phrase to the Sequence.
+  template<template <typename> class PhraseT, typename... Args>
+  SelfT& then( const T &value, Time duration, Args&&... args ) { _sequence.template then<PhraseT>( value, duration, std::forward<Args>(args)... ); return *this; }
+
+  /// Append a phrase to the Sequence.
+  SelfT& then( const PhraseRef<T> &phrase ) { _sequence.then( phrase ); return *this; }
+
+  /// Append a sequence to the Sequence.
+  SelfT& then( const Sequence<T> &sequence ) { _sequence.then( sequence ); return *this; }
+
+  //=================================================
+  // Extra Sugar.
+  //=================================================
+
+  /// Append a Hold to the end of the Sequence. Assumes you want to hold using the Sequence's current end value.
+  SelfT& hold( Time duration ) { _sequence.template then<Hold>( _sequence.getEndValue(), duration ); return *this; }
+
+  //=================================================
+  // Accessors to Motion and Sequence.
+  //=================================================
+
+  Sequence<T>& getSequence() { return _sequence; }
+  Motion<T>&   getMotion() { return _motion; }
+
+private:
+  Motion<T>       &_motion;
+  Sequence<T>     &_sequence;
+  const Timeline  &_timeline;
+};
+
+///
+/// CueOptions provide a facade for manipulating a timeline Cue.
+/// Non-get* methods return a reference back to the CueOptions object for chaining.
+/// Do not store the CueOptions object, as it contains a non-owning reference.
+///
+class CueOptions : public TimelineOptions<CueOptions>
+{
+public:
+  explicit CueOptions( Cue &cue ):
+  TimelineOptions<CueOptions>( cue ),
+  _cue( cue )
+  {}
+
+private:
+  Cue  &_cue;
+};
+
+} // namespace choreograph
