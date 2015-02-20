85ef83e3777fc62bb06e6b1e595714f4c9584f1e
David Wicks <wicks.db@gmail.com>
2015-02-20 12:17:03 -0500

    Test case based on a forum question.

--- tests/Choreograph_test.cpp
+++ tests/Choreograph_test.cpp
@@ -320,6 +320,27 @@ TEST_CASE( "Motion Groups" )
   }
 }
 
+TEST_CASE( "Forum Questions" )
+{
+  SECTION("Moving an Output updates the Motion target.")
+  {
+    // Based on Cinder forum issue:
+    // https://forum.libcinder.org/topic/bug-hang-in-timeline-replacetarget-function-infinite-loop#23286000002274021
+    ch::Timeline timeline;
+
+    ch::Output<float> oFloat[2] = { 0.0f, 0.0f };
+    timeline.apply( &oFloat[0] ).then<RampTo>( 1.0f, 1.0f );
+    timeline.apply( &oFloat[0] ).then<RampTo>( 2.0f, 1.0f ).removeOnFinish(true);
+    oFloat[1] = std::move(oFloat[0]);
+
+    timeline.jumpTo(1.0f);
+
+    REQUIRE(timeline.size() == 0);
+    REQUIRE(oFloat[0].value() == 0.0f);
+    REQUIRE(oFloat[1].value() == 2.0f);
+  }
+}
+
 //==========================================
 // Slicing and dicing
 //==========================================
