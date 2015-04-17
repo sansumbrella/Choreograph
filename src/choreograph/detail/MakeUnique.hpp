4de79b8df23e4b798005ffa1e8360fd36b94fcb7
David Wicks <wicks.db@gmail.com>
2015-04-17 15:43:10 -0400

    Always use our version of make_unique.
    Closes #30

--- src/choreograph/detail/MakeUnique.hpp
+++ src/choreograph/detail/MakeUnique.hpp
@@ -31,19 +31,24 @@ namespace choreograph
 {
 namespace detail
 {
-
+/*
+// Per github issue #30, Xcode Version 6.3 (6D570) breaks the template alias for unique_ptr.
+// We use our own make_unique template in all cases to avoid this issue.
 #if __cplusplus < 201402L
+*/
   // If you aren't using C++14, we define our own make_unique for choreograph::detail
   template<typename T, typename ...Args>
   std::unique_ptr<T> make_unique( Args&& ...args )
   {
     return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
   }
+/*
 #else
   // Use std::make_unique in the choreograph::detail namespace.
   template<typename T>
   using make_unique = std::make_unique<T>;
 #endif
+*/
 
 } // namespace detail
 } // namespace choreograph
