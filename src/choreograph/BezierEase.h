3c820bddee12140c70ef402dccb5d8d48d7d261e
David Wicks <david@sosolimited.com>
2016-01-11 14:07:54 -0500

    Bezier ease function creator.

--- src/choreograph/BezierEase.h
+++ src/choreograph/BezierEase.h
@@ -6,7 +6,17 @@
 //
 
 #pragma once
+#include "UnitBezier.h"
 
-namespace ChoreographTests {
+namespace choreograph {
 
-} // namespace ChoreographTests
+const auto createBezierEase(double x1, double y1, double x2, double y2) -> std::function<float (float)> {
+  auto bezier = BezierInterpolant(x1, y1, x2, y2);
+  auto epsilon = std::numeric_limits<float>() * 100;
+
+  return [bezier, epsilon] (float t) {
+    return bezier.solve(t, epsilon);
+  };
+}
+
+} // namespace choreograph
