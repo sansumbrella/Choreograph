89053502ea2dc78fc891affb5363a7a87853cb17
David Wicks <david@sosolimited.com>
2016-01-11 14:11:48 -0500

    Initial tests for channels and bezier curve.

--- tests/Channel_test.cpp
+++ tests/Channel_test.cpp
@@ -6,14 +6,38 @@
 //
 
 #include "catch.hpp"
+#include "Channel.hpp"
 
-TEST_CASE("Channel_testTest")
+using namespace choreograph;
+
+TEST_CASE("Channel")
 {
-	auto a = 0;
-	auto b = 1;
 
-	SECTION("Write a failing test first to confirm your new test is run.")
-	{
-		REQUIRE(a == b);
-	}
+  SECTION("Simple channel interpolation")
+  {
+    auto channel = Channel<float>();
+    channel.appendKeyAfter(0.0f, 0)
+    .appendKeyAfter(10.0f, 1.0)
+    .appendKeyAfter(5.0f, 1.0);
+
+    REQUIRE(channel.index(-0.5) == 0);
+    REQUIRE(channel.index(0.5) == 0);
+    REQUIRE(channel.index(1.0) == 0);
+    REQUIRE(channel.index(1.1) == 1);
+    REQUIRE(channel.index(5.5) == 1);
+  }
+
+  SECTION("Default bezier handles result in effectively linear interpolation.")
+  {
+    auto bezier = BezierInterpolant();
+
+    for (auto t = 0.0f; t <= 1.0f; t += 0.05f) {
+      CHECK(bezier.curveX(t) == Approx(t));
+      CHECK(bezier.curveY(t) == Approx(t));
+    }
+
+    auto solution = bezier.solve(0.5f, std::numeric_limits<float>::epsilon());
+    REQUIRE(solution == Approx(0.5));
+  }
+
 }
