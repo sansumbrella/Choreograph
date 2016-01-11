8a2318da6ae28696b9cad486d1eaf7f437d301d4
David Wicks <david@sosolimited.com>
2016-01-11 14:07:38 -0500

    Unit bezier derived from Webkit.

---
+++ src/choreograph/BezierEase.h
@@ -0,0 +1,12 @@
+//
+//  BezierEase.h
+//
+//  Created by Soso Limited on 1/11/16.
+//
+//
+
+#pragma once
+
+namespace ChoreographTests {
+
+} // namespace ChoreographTests
---
+++ src/choreograph/Channel.cpp
@@ -0,0 +1,10 @@
+//
+//  Channel.cpp
+//
+//  Created by Soso Limited on 1/11/16.
+//
+//
+
+#include "Channel.h"
+
+using namespace soso;
